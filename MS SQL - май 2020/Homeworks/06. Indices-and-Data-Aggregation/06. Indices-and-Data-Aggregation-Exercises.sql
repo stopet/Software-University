acab66572693dd6cba934e38ffc0e789606d8851
stopet <petkov.sn@gmail.com>
2020-06-11 13:57:46 +0300

    Update

--- MS SQL - май 2020/Homeworks/06. Indices-and-Data-Aggregation/06. Indices-and-Data-Aggregation-Exercises.sql
+++ MS SQL - май 2020/Homeworks/06. Indices-and-Data-Aggregation/06. Indices-and-Data-Aggregation-Exercises.sql
@@ -12,3 +12,27 @@ SELECT DepositGroup,MAX(MagicWandSize) AS LongestMagicWand FROM WizzardDeposits
 SELECT TOP(2) DepositGroup FROM WizzardDeposits
 	GROUP BY DepositGroup
 	ORDER BY AVG(MagicWandSize)
+
+--Problem 5
+SELECT DepositGroup,SUM(DepositAmount) AS TotalSum FROM WizzardDeposits
+	GROUP BY DepositGroup
+
+--Problem 6
+SELECT DepositGroup,SUM(DepositAmount) AS TotalSum FROM WizzardDeposits
+	WHERE MagicWandCreator='Ollivander family'
+	GROUP BY DepositGroup
+
+--Problem 7
+SELECT DepositGroup,SUM(DepositAmount) AS TotalSum FROM WizzardDeposits
+	WHERE MagicWandCreator='Ollivander family'
+	GROUP BY DepositGroup
+	HAVING SUM(DepositAmount)<150000
+	ORDER BY TotalSum DESC
+
+--Problem 8
+SELECT DepositGroup,MagicWandCreator,MIN(DepositCharge) AS MinDepositCharge FROM WizzardDeposits
+	GROUP BY DepositGroup,MagicWandCreator
+	ORDER BY MagicWandCreator,DepositGroup
+
+--Problem 9
+	
