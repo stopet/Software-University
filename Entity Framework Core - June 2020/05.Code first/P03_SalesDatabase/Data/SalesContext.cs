8b48ca55e1b350c60031087554a2607a4cc8ea8d
stopet <bjvarna@gmail.com>
2020-07-22 16:04:54 +0300

    CodeFirst

---
+++ Entity Framework Core - June 2020/05.Code first/P03_SalesDatabase/Data/SalesContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P03_SalesDatabase.Data.Config;
+using P03_SalesDatabase.Data.Models;
+
+namespace P03_SalesDatabase.Data
+{
+    public class SalesContext : DbContext
+    {
+        public SalesContext()
+        {
+
+        }
+
+        public SalesContext(DbContextOptions dbContextOptions)
+            : base(dbContextOptions)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Connection.ConnectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Name)
+                .IsUnicode();
+            });
+
+            modelBuilder.Entity<Customer>(entity=>
+            {
+                entity.Property(e => e.Name)
+                .IsUnicode();
+            })
+        }
+    }
+}
