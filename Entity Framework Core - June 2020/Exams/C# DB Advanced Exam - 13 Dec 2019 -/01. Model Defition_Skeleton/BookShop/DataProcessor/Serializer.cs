0e3a11e67fce97b2c657133b056bb637406af83f
stopet <bjvarna@gmail.com>
2020-08-07 16:16:00 +0300

    exam resolved

--- Entity Framework Core - June 2020/Exams/C# DB Advanced Exam - 13 Dec 2019 -/01. Model Defition_Skeleton/BookShop/DataProcessor/Serializer.cs
+++ Entity Framework Core - June 2020/Exams/C# DB Advanced Exam - 13 Dec 2019 -/01. Model Defition_Skeleton/BookShop/DataProcessor/Serializer.cs
@@ -7,6 +7,8 @@
     using System.Text;
     using System.Xml;
     using System.Xml.Serialization;
+    using BookShop.Data.Models.Enums;
+    using BookShop.DataProcessor.ExportDto;
     using Data;
     using Newtonsoft.Json;
     using Formatting = Newtonsoft.Json.Formatting;
@@ -15,12 +17,60 @@
     {
         public static string ExportMostCraziestAuthors(BookShopContext context)
         {
-            throw new NotImplementedException();
+            var mostCraziestAuthors = context
+                .Authors
+                .Select(a => new
+                {
+                    AuthorName = a.FirstName + ' ' + a.LastName,
+                    Books = a.AuthorsBooks
+                    .OrderByDescending(ab => ab.Book.Price)
+                        .Select(ab => new
+                        {
+                            BookName = ab.Book.Name,
+                            BookPrice = ab.Book.Price.ToString("f2")
+                        })
+                        .ToArray()
+                })
+                .ToArray()
+                .OrderByDescending(a => a.Books.Count())
+                .ThenBy(a => a.AuthorName)
+                .ToArray();
+
+            string json = JsonConvert.SerializeObject(mostCraziestAuthors, Formatting.Indented);
+
+            return json;
         }
 
         public static string ExportOldestBooks(BookShopContext context, DateTime date)
         {
-            throw new NotImplementedException();
+            StringBuilder sb = new StringBuilder();
+
+            ExportBookDto[] books = context
+                .Books
+                .Where(b => b.PublishedOn < date && b.Genre == Genre.Science)
+                .ToArray()
+                .OrderByDescending(b => b.Pages)
+                .ThenByDescending(b => b.PublishedOn)
+                .Take(10)
+                .Select(b => new ExportBookDto
+                {
+                    Name = b.Name,
+                    Date = b.PublishedOn.ToString("d",CultureInfo.InvariantCulture),
+                    Pages = b.Pages
+                })
+                .ToArray();
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof
+                (ExportBookDto[]),new XmlRootAttribute("Books"));
+            XmlSerializerNamespaces namespaces = new XmlSerializerNamespaces();
+            namespaces.Add(string.Empty, string.Empty);
+
+            using (StringWriter stringWriter=new StringWriter(sb))
+            {
+                xmlSerializer.Serialize(stringWriter, books, namespaces);
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
